aeea0af4246346967e6df13e1ffd1ba305de51d0
Haj B <phajib@gmail.com>
2019-09-02 01:28:31 -0700

    added :updated_at

--- db/migrate/20190831073836_create_logs.rb
+++ db/migrate/20190831073836_create_logs.rb
@@ -5,6 +5,7 @@ class CreateLogs < ActiveRecord::Migration
       t.string :shoot_title
       t.datetime :shoot_date
       t.string :notes
+      t.datetime :updated_at
 
       t.timestamps null: false
     end
