a20e109dd25ba9389ce7953aff4fdc2482fc75f3
Haj B <phajib@gmail.com>
2019-09-05 00:43:51 -0700

    UsersController get & post login and use UsersController added to config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+    get '/login' do
+        erb :login
+    end
+
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            flash[:message] = "Welcome back, #{@user.name}!"
+            redirect "users/#{user.id}"
+        else
+            flash[:errors] = "Your credentials were invalid.  Please sign up or try again."
+            redirect '/login'
+        end
+    end
+    
     get '/signup' do
         if logged_in?
           redirect to '/clic'
