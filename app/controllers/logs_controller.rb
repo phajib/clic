ead62f2b7678dd8691ac9686fee0a11fddf64d92
Haj B <phajib@gmail.com>
2019-09-10 03:49:53 -0700

    logs routes

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -14,10 +14,58 @@ class LogsController < ApplicationController
     post '/logs' do
         redirect_if_not_logged_in
         if params[:shoot_title] || params[:shoot_date] || params[:place] || params[:city] || params[:country] || params[:camera] || params[:lens] || params[:speedlight] || params[:drone] || params[:accessories] || params[:notes] != ""
-            @logs = logs.create(content: params[:content],book_title: params[:book_title], user_id: current_user.id)
-            redirect "/logs/#{@journal_entry.id}"
+            @logs = logs.create(params)
+            redirect "/logs/#{@logs.id}"
         else
             redirect '/logs/new'
         end
+    end
+
+    get '/logs/:id' do
+        find_entry
+        erb :'/logs/show'
+    end
+
+    get '/logs/:id/edit' do
+        find_entry
+        if logged_in?
+            if @logs.user == current_user
+                erb :'/logs/edit'
+            else
+                redirect "users/#{current_user.id}"
+            end
+        else
+            redirect "/"
+        end
+    end
+
+    patch '/logs/:id' do
+        find_entry
+        if logged_in?
+            if @logs.user == current_user
+                @logs.update(params) 
+                redirect "/logs/#{@logs.id}"
+            else
+                redirect "users/#{current_user.id}"
+            end
+        else
+            redirect "/"
+        end
+    end
+
+    delete '/logs/:id' do
+        find_entry
+        if @logs.user == current_user
+            @logs.destroy
+            redirect '/logs'
+        else
+            redirect '/logs'
+        end
+    end
+
+    #helper method to prevent code duplication
+    private #this method will only be used here
+    def find_entry
+        @logs = Logs.find(params[:id])
     end
 end
