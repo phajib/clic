51f57dfd3dcfe6df0b571a631d5d5d245d20897e
Haj B <phajib@gmail.com>
2019-09-10 01:53:01 -0700

    post /logs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,8 @@
 class LogsController < ApplicationController
 
     get '/logs' do
+        redirect_if_not_logged_in
+        @user = current_user
         @logs = Logs.all
         erb :'/logs/index.html'
     end
@@ -11,13 +13,11 @@ class LogsController < ApplicationController
 
     post '/logs' do
         redirect_if_not_logged_in
-            if params[:content] || params[:book_title] != ""
-                @journal_entry = JournalEntry.create(content: params[:content], book_title: params[:book_title], user_id: current_user.id)
-                redirect "/journal_entries/#{@journal_entry.id}"
-            else
-                redirect '/journal_entries/new'
-            end
+        if params[:shoot_title] || params[:shoot_date] || params[:place] || params[:city] || params[:country] || params[:camera] || params[:lens] || params[:speedlight] || params[:drone] || params[:accessories] || params[:notes] != ""
+            @logs = logs.create(content: params[:content],book_title: params[:book_title], user_id: current_user.id)
+            redirect "/logs/#{@journal_entry.id}"
+        else
+            redirect '/logs/new'
         end
-
     end
 end
